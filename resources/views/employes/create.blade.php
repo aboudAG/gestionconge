12afd8b66b90fba668320c102a05deeaec0b102a
abdenour <abdenour.agag@gmail.com>
2024-05-03 17:13:58 +0100

    10th

--- resources/views/employes/create.blade.php
+++ resources/views/employes/create.blade.php
@@ -6,11 +6,11 @@
     </x-slot>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <!-- Lien CSS pour Choices.js -->
+    {{-- <!-- Lien CSS pour Choices.js -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/choices.js/public/assets/styles/choices.min.css">
 
 <!-- Script JavaScript pour Choices.js -->
-<script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/choices.js/public/assets/scripts/choices.min.js"></script> --}}
 
 
 
@@ -170,6 +170,11 @@
                                           required autocomplete="new-password" />
                             <span toggle="#password" class="fa fa-fw fa-eye field-icon toggle-password"></span>
                             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                        </div>
+                        <div>
+                            <x-input-label for="name" :value="__('Name')" style="display : none;" />
+                            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" style="display : none;"  />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
                         <div class="col-12">
                             <button type="submit" class="btn btn-primary">Ajouter l'employé</button>
