d9bb7ca941c9af78a50de14cd0636934ebae3315
rachid <rachidscorpionzor@gmail.com>
2024-05-03 17:59:38 +0100

    commit12

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,14 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-6 text-gray-900" style="display:flex; flex-direction:column;">
                     {{ __("You're logged in!") }}
+                    <a href="{{ route('employes.create') }}" class="btn btn-primary">Ajouter un nouvel employé</a>
+                    <a href="{{ route('employes.index') }}" class="btn btn-primary">Liste employé</a>
+                    <a href="{{ route('demandes.create') }}" class="btn btn-primary">Demande Congé</a>
+                    
                 </div>
+                
             </div>
         </div>
     </div>
@@ -19,5 +24,5 @@
 
 
 
-    <a href="{{ route('employes.create') }}" class="btn btn-primary">Ajouter un nouvel employé</a>
+ 
 </x-app-layout>
